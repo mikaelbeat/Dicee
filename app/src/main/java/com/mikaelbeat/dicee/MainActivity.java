783475a9d2e0ceefe42a5b2f97d76ab239c00f00
Ryynanen, Petri <petri.ryynanen@accenture.comgit clone https://petri.ryynanen@innersource.accenture.com/scm/afhrtvkn/hrrt-test-scripts.gitgit config --global user.email petri.ryynanen@accenture.com>
2018-07-09 00:41:09 +0300

    Refactoring code

--- app/src/main/java/com/mikaelbeat/dicee/MainActivity.java
+++ app/src/main/java/com/mikaelbeat/dicee/MainActivity.java
@@ -11,7 +11,6 @@ import android.widget.ImageView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
     
     MediaPlayer backgroundMusic;
     MediaPlayer rollButtonSound;
